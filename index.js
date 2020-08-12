ffd611c3b833b6c6f7e9d1d29c9c2e79d3822807
Chamath <chamathvin@gmail.com>
2020-08-12 19:46:19 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send("<h1>This is sourced from the new github repo</h1>");
+});
+
+app.listen(8080, () => {
+    console.log(result);
+});
