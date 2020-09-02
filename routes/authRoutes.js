b79b5bfbb91dae89a605bd9d6c2579eed768e7c5
Chamath <chamathvin@gmail.com>
2020-09-02 15:47:13 +1000

    added flash messages

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,14 @@
 const bcrypt = require('bcrypt');
 const express = require('express');
 const models = require('../models/index');
-const messages = require('../utils/messageList');
 const passportConfig = require('../auth/passport');
 
 // An express router to better handle routes
 let Router = express.Router();
 
 // Route for retrieving the login page.
-Router.get('/login/:error', (req, res, next) => {
-    res.render('login', { message: messages.login[req.params.error] }, (err, html) => {
+Router.get('/login', (req, res, next) => {
+    res.render('login', { message: req.flash('error') }, (err, html) => {
         if(err)
             throw err;
         else
@@ -21,20 +20,20 @@ Router.get('/login/:error', (req, res, next) => {
 // This uses the authenticate method exposed by passport through the
 // predefined local strategy.
 Router.post('/login', 
-    passportConfig.passport.authenticate('local', { failureRedirect: '/login/incorrect_credentials' }),
+    passportConfig.passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
     (req, res, next) => {
         res.redirect('/user_dashboard', { user: req.user });
     }
 );
 
 // Route for retrieving the register page
-Router.get('/register/:error', (req, res, next) => {
-    res.render('/register', { message: messages.register[req.params.error] });
+Router.get('/register', (req, res, next) => {
+    res.render('/register', { message: req.flash('signupError') });
 });
 
 // Route to register a user.
 // Bcrypt is used to hash the user password wuth the said of salting
-Router.post('/register/:error', (req, res, next) => {
+Router.post('/register', (req, res, next) => {
     let firstName = req.body.firstName;
     let lastName = req.body.lastName;
     let email = req.body.email;
@@ -52,7 +51,7 @@ Router.post('/register/:error', (req, res, next) => {
                             .then(user => {
                                 req.login(user, (err) => {
                                     if(err) throw err;
-                                    else res.render('/user_dashboard', { user: user });
+                                    else res.render('/user_dashboard', { user: req.user });
                                 });
                             });
                         }else{
@@ -64,7 +63,8 @@ Router.post('/register/:error', (req, res, next) => {
                 }
             });
         }else{
-            res.redirect('/register/user_exists');
+            req.flash('signupError', 'Sorry there is already an account for this email');
+            res.redirect('/register');
         }
     })
     .catch(err => {
